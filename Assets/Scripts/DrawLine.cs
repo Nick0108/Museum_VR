ea39fbc3c846e5dcfdc9f194bf8534610e13fa3c
Nick0108 <zidonglin@outlook.com>
2017-07-09 12:21:33 +0800

    Feat: Draw a line beween the camera and the ball(Level_1)

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public Transform startLine;
+    public Transform EndLine;
+    
+    private LineRenderer Line;
+
+    private void Start()
+    {
+        Line = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        //Line.SetPosition(0, startLine.position);
+        Line.SetPosition(2,new Vector3(EndLine.position.x,0.03f, EndLine.position.z));
+        Line.SetPosition(3, EndLine.position);
+    }
+
+}
