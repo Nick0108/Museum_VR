90ac59d5c05dde16036102b8f84ae086a94ddd70
Nick0108 <zidonglin@outlook.com>
2017-07-13 13:01:44 +0800

    Feat: Add the read Function

---
+++ Assets/Scripts/Read.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Read : MonoBehaviour {
+
+    public MyGameManager myGameManager;
+
+    private void Start()
+    {
+        myGameManager = GetComponent<MyGameManager>();
+    }
+
+    public void HasRead()
+    {
+        gameObject.SetActive(true);
+        myGameManager.ReadNum++;
+        Debug.Log("ReadNum: "+ myGameManager.ReadNum);
+        myGameManager.SetLevel();
+    }
+}
